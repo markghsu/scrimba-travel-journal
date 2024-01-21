da65bf160a8c33212518bf6aaa961935b3fe5b76
Mark Hsu <mark.h@canelasoftware.com>
2024-01-21 00:02:35 -0800

    Add data and update DestinationSection to work with props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import DestinationSection from './DestinationSection'
+import data from '../data'
 import './App.css'
 
 export default function App() {
     return (
         <div className="app">
             <Header />
-            <DestinationSection />
-            <DestinationSection />
-            <DestinationSection />
+            <div class="container">
+            {
+                data.map((e) => <DestinationSection {...e} />)
+            }
+            </div>
         </div>
     )
 }
--- src/components/DestinationSection.jsx
+++ src/components/DestinationSection.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import mapIcon from '../assets/map_icon.svg'
 import './DestinationSection.css'
 
-export default function DestinationSection() {
+export default function DestinationSection({title, location, googleMapsUrl, startDate, endDate, description, imageUrl}) {
     return (
         <section className="destination-section">
-            <img src="https://source.unsplash.com/WLxQvbMyfas" className="destination--image" alt="Mount Fuji" />
+            <img src={imageUrl} className="destination--image" alt={title} />
             <div className="destination--info">
-                <h1 className="destination--name">Mount Fuji</h1>
+                <h1 className="destination--name">{title}</h1>
                 <div className="destination--location">
-                    <h4 className="destination--location--country"><img src={mapIcon} alt="" className="destination--location--icon" /> Japan</h4>
-                    <a className="destination--location--link" href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on Google Maps</a>
+                    <h4 className="destination--location--country"><img src={mapIcon} alt="" className="destination--location--icon" /> {location}</h4>
+                    <a className="destination--location--link" href={googleMapsUrl}>View on Google Maps</a>
                 </div>
-                <h3 className="destination--dates">12 Jan, 2021 - 24 Jan 2021</h3>
-                <p className="destination--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h3 className="destination--dates">{startDate} - {endDate}</h3>
+                <p className="destination--description">{description}</p>
             </div>
         </section>
     )
